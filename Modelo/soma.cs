37714004a08727f70e56b683567dcd9f850126b4
aphorbux <162336280+aphorbux@users.noreply.github.com>
2024-05-04 18:20:23 +0000

    fisrt commit

---
+++ Modelo/soma.cs
@@ -0,0 +1,8 @@
+namespace Modelo{
+
+    public static class Adicao{
+        public static double Somar(double n1, double n2){
+            return n1 + n2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Modelo;
+
+/*              Questão 1
+double N1 = Convert.ToDouble(Console.ReadLine());
+double N2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Adicao.Somar(N1, N2));
+*/
+
+
+
+/*              Questão 3
+double N1 = Convert.ToDouble(Console.ReadLine());
+double N2 = Convert.ToDouble(Console.ReadLine());
+double N3 = Convert.ToDouble(Console.ReadLine());
+
+Maximus.Max(N1, N2, N3);
+*/
